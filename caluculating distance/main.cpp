98eaa70e40b6be5e1ad091bb1385dbff8562e93c
kaibalya sahoo <120CR0406@nitrkl.ac.in>
2021-03-04 08:25:08 +0530

    Initial Commit

---
+++ caluculating distance/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  caluculating distance
+//
+//  Created by KAIBALYA SAHOO on 13/12/1942 Saka.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
